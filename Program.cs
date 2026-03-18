b018d0d9b3e21ab5d488d617f3a8d42d4014c4e1
prvabeogim <prvabeogim@outlook.com>
2026-03-18 10:21:07 +0100

    V 1.0 Login

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Projekat_2026_Stevan_Matic_A
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
